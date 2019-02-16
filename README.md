f33c6cd2f82f1d4ff7f1d3c8970b4a2f8688c785
Nico Schilling <nico.schilling@nordcloud.com>
2019-02-16 23:42:06 +0100

    Optimization

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # AWS S3 Object Count
 
-With this code snipped you can check the object count of an AWS S3 Bucket via HTTP API. For production use you should think about authentication at the HTTP endpoint.
+With this solution, you can check the object count of an AWS S3 Bucket via HTTP API.
 
 You can use this HTTP endpoint to integrate the object count in a monitor system. It can be used to check S3 replication for example.
 
 ## Requirements
 
 * Python 3 (tested with version 3.7.0)
-* Node.js (tested with version 8.12.0)
-* Serverless (tested with version 1.32.0)
-* AWS Pseudo Parameters (install via ```npm install serverless-pseudo-parameters```)
-* serverless plugin tracing (install via ```npm install serverless-plugin-tracing```)
+* Node.js (tested with version 10.15.1)
+* Serverless (tested with version 1.37.1)
+* Serverless Plugins
+  * AWS Pseudo Parameters (install via ```npm install serverless-pseudo-parameters```)
+  * serverless plugin tracing (install via ```npm install serverless-plugin-tracing```)
+  * serverless-api-gateway-xray plugin (install via npm install serverless-api-gateway-xray)
 
-## How to install
+## Deploy the solution
 
 1. Clone this repository
 2. Deploy the solution to AWS with ```sls deploy```. Please note the API-ID and the X-API-Key in the output
 3. Test the endpoint via ```curl -X GET 'https://<<gateway id>>.execute-api.<<region>>.amazonaws.com/dev/?s3BucketName=<<<bucket-name>>>' --header "X-Api-Key:<<<API-Key>>>"```
+
+### Undeploy the solution
+
+Just follow the next steps to undeploy the solution:
+
+1. run ```sls remove``` to remove the solution
--- handler.py
+++ handler.py
@@ -1,59 +1,82 @@
-from datetime import datetime, timedelta
 import json
 import boto3
+import logging
+from datetime import datetime, timedelta
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3_resource = boto3.resource('s3')
+cloudwatch_client = boto3.client('cloudwatch')
+
+error_response = {
+    "statusCode": 500,
+    "headers": {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': True,
+    },
+    "body": "An error occurred. Please contact the Administrator."
+}
+
+def getS3ObjectCountFromCloudWatch(s3bucketName):
+    logger.info('getS3ObjectCountFromCloudWatch for bucket ' + s3bucketName)
+    get_metric_data_response = cloudwatch_client.get_metric_data(
+        MetricDataQueries=[
+            {
+                'Id': 'm1',
+                'MetricStat': {
+                    'Metric': {
+                        'Namespace': 'AWS/S3',
+                        'MetricName': 'NumberOfObjects',
+                        'Dimensions': [
+                            {
+                                'Name': 'StorageType',
+                                'Value': 'AllStorageTypes'
+                            },
+                            {
+                                'Name': 'BucketName',
+                                'Value': s3bucketName
+                            },
+                        ]
+                    },
+                    'Period': 3600,
+                    'Stat': 'Average',
+                    'Unit': 'Count'
+                },
+                'ReturnData': True
+            },
+        ],
+        StartTime=datetime.now() - timedelta(days=1),
+        EndTime=datetime.now()
+    )
+
+    s3ObjectCount = get_metric_data_response['MetricDataResults'][0]['Values'][0]
+    logger.info('Found ' + str(s3ObjectCount) + ' objects in the bucket')
+    return s3ObjectCount
+
+def checkS3BucketExists(s3bucketName):
+    logger.info('checkS3BucketExists for bucket ' + s3bucketName)
+    bucketExists = s3_resource.Bucket(s3bucketName) in s3_resource.buckets.all()
+    return bucketExists
 
 def awss3objectcount(event, context):
-
     try:
         s3bucketName = event['multiValueQueryStringParameters']['s3BucketName'][0]
     except:
-        response = {
-            "statusCode": 500,
-            "headers": {
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Credentials': True,
-            },
-            "body": "An error occurred. Please contact the Administrator."
-        }
-        print("ERROR: The parameter s3BucketName was not provided.")
-        return response
-
-    s3 = boto3.resource('s3')
-    bucketExists = s3.Bucket(s3bucketName) in s3.buckets.all()
+        logger.error("The parameter s3BucketName was not provided in the event.")
+        return error_response
+    try:
+        bucketExists = checkS3BucketExists(s3bucketName)
+    except:
+        logger.error("Could not check if the bucket exists")
+        return error_response
 
     if bucketExists:
-        client = boto3.client('cloudwatch')
-        response = client.get_metric_data(
-            MetricDataQueries=[
-                {
-                    'Id': 'm1',
-                    'MetricStat': {
-                        'Metric': {
-                            'Namespace': 'AWS/S3',
-                            'MetricName': 'NumberOfObjects',
-                            'Dimensions': [
-                                {
-                                    'Name': 'StorageType',
-                                    'Value': 'AllStorageTypes'
-                                },
-                                {
-                                    'Name': 'BucketName',
-                                    'Value': s3bucketName
-                                },
-                            ]
-                        },
-                        'Period': 3600,
-                        'Stat': 'Average',
-                        'Unit': 'Count'
-                    },
-                    'ReturnData': True
-                },
-            ],
-            StartTime=datetime.now() - timedelta(days=1),
-            EndTime=datetime.now()
-        )
-
-        s3ObjectCount = response['MetricDataResults'][0]['Values'][0]
+        try:
+            s3ObjectCount = getS3ObjectCountFromCloudWatch(s3bucketName)
+        except:
+            logger.error("Could not retrieve s3 object count from cloudwatch")
+            return error_response
 
         body = {
            "s3BucketName": s3bucketName,
@@ -68,14 +91,7 @@ def awss3objectcount(event, context):
             },
             "body": json.dumps(body)
         }
+        return response
     else:
-        response = {
-            "statusCode": 500,
-            "headers": {
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Credentials': True,
-            },
-            "body": "An error occurred. Please contact the Administrator."
-        }
-        print('ERROR: The S3 bucket ' + s3bucketName + ' was not provided or the S3 connection could not be established.')
-    return response
+        logger.error('The S3 bucket ' + s3bucketName + ' was not provided or the S3 connection could not be established.')
+    return error_response
